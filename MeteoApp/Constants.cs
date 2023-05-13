0c103055602cd927a63b050658371c9a6c0de967
alessioBertollo <alessio-bertollo@live.com>
2023-05-13 18:58:09 +0200

    Add the functions for the weatherAPI calling and I add the dependencies for the geolocation library and JsonParser for the store of the data from the API call

---
+++ MeteoApp/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeteoApp
+{
+    internal class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "cc76872429cb738c57ebaf64163a7534";
+    }
+}
--- MeteoApp/MeteoItemPage.xaml.cs
+++ MeteoApp/MeteoItemPage.xaml.cs
@@ -3,6 +3,7 @@
 [QueryProperty(nameof(Entry), "Entry")]
 public partial class MeteoItemPage : ContentPage
 {
+    RestService _restService;
     Entry entry;
     public Entry Entry
     {
@@ -17,7 +18,28 @@ public partial class MeteoItemPage : ContentPage
     public MeteoItemPage()
     {
         InitializeComponent();
+        _restService = new RestService();
         BindingContext = this;
+    }
+
+    async void OnGetWeatherButtonClicked(object sender, EventArgs e)
+    {
+        if (!string.IsNullOrWhiteSpace("lat=46.5386&lon=10.1357"))
+        {
+            WeatherData weatherData = await
+                _restService.
+                GetWeatherData(GenerateRequestURL(Constants.OpenWeatherMapEndpoint));
+
+            BindingContext = weatherData;
+        }
+    }
+    string GenerateRequestURL(string endPoint)
+    {
+        string requestUri = endPoint;
+        requestUri += $"?q={"lat=46.5386&lon=10.1357"}";
+        requestUri += "&units=metric";
+        requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
+        return requestUri;
     }
 
     protected override void OnAppearing()
